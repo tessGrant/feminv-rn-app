973bfef64fc6c2c3746ac194e0f0bc53ab68c029
TessGrant <taisa.grankina@gmail.com>
2024-02-09 12:23:29 +0100

    feat: expo-router adds

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my new app!</Text>
+      <Text>super star!</Text>
       <StatusBar style='auto' />
     </View>
   );
